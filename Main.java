c3813a17186c3110b74d81485b656c5bcde9a97d
SuyashVJain09 <suyashvasaljain09@gmail.com>
2024-06-16 19:56:25 +0530

    Edit 2

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
         public static void main(String[] args) {
             System.out.println("Hello World");
+            String name = "Suyash";
+            byte age = 17;
+            long mobile = 9343379736L;
+            System.out.println("First letterof name: "+name.charAt(0));
+            System.out.println("Name: "+name+" Length of name: " +name.length());
+            System.out.println("Last 4 letter of name: " + name.substring(2,6));
+
         }
-      
-}
+    }
