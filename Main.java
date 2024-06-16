5e1f90f23cd5bac77e395f26e032012a1ec846b7
SuyashVJain09 <suyashvasaljain09@gmail.com>
2024-06-16 20:30:42 +0530

    edit 3

--- Main.java
+++ Main.java
@@ -1,12 +1,35 @@
+
+import java.util.Arrays;
+
 public class Main {
         public static void main(String[] args) {
             System.out.println("Hello World");
             String name = "Suyash";
             byte age = 17;
             long mobile = 9343379736L;
+            /*
             System.out.println("First letterof name: "+name.charAt(0));
             System.out.println("Name: "+name+" Length of name: " +name.length());
             System.out.println("Last 4 letter of name: " + name.substring(2,6));
-
+            */
+            System.out.println(" ");
+            System.out.println("Name: "+name );
+            System.out.println("Age: "+age );
+            System.out.println("Mobile no.: "+mobile );
+            System.out.println(" ");
+            /*
+            int[] marks = new int[5];
+            marks[0]=100;
+            marks[1]=96;
+            marks[2]=65;
+            marks[3]=16;
+            marks[4]=95;
+            */
+            int[] marks= {100,96,65,16,95};
+            int[][] marksfor2={{74,85,96,78,89},{96,85,74,98,87}};
+            System.out.println(marks.length);
+            Arrays.sort(marks);
+            System.out.println(marks[4]);
+            System.out.println(marksfor2[1][3]);
         }
     }
